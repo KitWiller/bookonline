474b9de2cd94a439ce72230e35ead52f5df91c02
gmanfrini <48986427+gmanfrini@users.noreply.github.com>
2021-04-16 10:35:40 +0200

    Impostato caricamento libri home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,33 @@
 import React from "react"
+import { StaticQuery, graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-export default function Home() {
-	return (
-		<Layout>
-            index
-		</Layout>
-	)
-}
 
+const Home = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allBookstoreJson {
+          edges {
+            node {
+              title
+              isbn10
+              isbn13
+              image
+              rating
+              price
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Layout location="/destinations" title="Destinations">
+          <div>{JSON.stringify(data.allBookstoreJson.edges)}</div>
+        </Layout>
+      </>
+    )}
+  />
+)
 
+export default Home
