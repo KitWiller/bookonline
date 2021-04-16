42d2fac6d9de97fe79b7b228d5999cadc72a21df
KitWiller <81360843+KitWiller@users.noreply.github.com>
2021-04-16 15:20:23 +0200

    inserito prezzo su index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import StarRating from "../components/starrating"
-
+import Immagine from "../components/immagine"
 
 const Home = props => (
   <StaticQuery
@@ -57,7 +57,9 @@ function nodeView(nodi){                                                   //fun
         title={"link to " + libro.title }>
         {libro.title}
         <StarRating rating = {libro.rating} />
+        {libro.price}
         </Link>
+        
        
        
        
